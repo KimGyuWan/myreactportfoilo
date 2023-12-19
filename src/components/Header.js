a8ab0a28a2668f88668a146b1fe6d13298ccc951
KimGyuWan <y5g5776@naver.com>
2023-12-19 20:57:07 +0900

    상단 header 및 navigation 기초 작업 완료

---
+++ src/components/Header.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './header.scss';
+
+const Header = () => {
+  const [isSiteVisible, setSiteVisible] = useState(false);
+  const [showUl, setShowUl] = useState(false);
+  const [showListIcon, setShowListIcon] = useState(true);
+  const siteMenuRef = useRef(null);
+
+  const toggleSiteVisibility = () => {
+    setSiteVisible(!isSiteVisible);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const breakpoint = 1000;
+      setShowUl(window.innerWidth >= breakpoint);
+      setShowListIcon(window.innerWidth < breakpoint);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleSiteMenu = () => {
+    const siteMenu = siteMenuRef.current;
+    if (siteMenu) {
+      siteMenu.style.display = siteMenu.style.display === 'none' ? 'block' : 'none';
+    }
+  };
+
+  return (
+    <>
+      {showListIcon && (
+        <div className='site' style={{ display: isSiteVisible ? 'block' : 'none' }}>
+          <a href="#none">
+            <div className='login d-flex justify-content-between'>
+              <span>로그인이 필요합니다.</span>
+              <button>로그인</button>
+            </div>
+          </a>
+          <ul className='login_gnb'>
+            <li><a href="#none"><span>메뉴추천가이드</span></a></li>
+            <li><div className='d-flex justify-content-between'><a href="#none"><span>메뉴안내</span></a>
+              <span className='chevron' onClick={toggleSiteMenu}></span></div>
+              <ul ref={siteMenuRef} className='site_menu'>
+                <li><a href="#none">1리터 대용량</a></li>
+                <li><a href="#none">1.1리터 보틀</a></li>
+                <li><a href="#none">COFFEE</a></li>
+                <li><a href="#none">NON COFFEE</a></li>
+                <li><a href="#none">디카페인</a></li>
+                <li><a href="#none">생과일 주스&티</a></li>
+                <li><a href="#none">스무디&프라페</a></li>
+                <li><a href="#none">디저트</a></li>
+                <li><a href="#none">디저트(배달전용메뉴)</a></li>
+              </ul>
+            </li>
+            <li><a href="#none"><span>매장찾기</span></a></li>
+          </ul>
+          {isSiteVisible && (
+            <div className='closebtn'>
+              <button onClick={toggleSiteVisibility}><i class="bi bi-x-lg"></i></button>
+            </div>
+          )}
+        </div>
+      )}
+      <header id='hd' className='mycontainer mx-auto justify-content-between d-flex align-items-center'>
+        <h1><a href=""></a></h1>
+        {showUl && (
+          <ul className='d-flex justify-content-center align-items-center'>
+            <li className='px-2'><a href="#none"><span>메뉴추천가이드</span></a></li>
+            <li className='px-2'><a href="#none"><span>베스트메뉴</span></a></li>
+            <li className='px-2'><a href="#none"><span>매장찾기</span></a></li>
+          </ul>
+        )}
+        <div className='d-flex'>
+          <span className='headericon'><i className="bi bi-search"></i></span>
+          {showListIcon && (
+            <div>
+              <span className='headericon pl-4' onClick={toggleSiteVisibility}><i className="bi bi-list"></i></span>
+            </div>
+          )}
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
+
