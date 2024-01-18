3b94f8c58efa0a985f14b14140042c173adf5a52
KimGyuWan <y5g5776@naver.com>
2024-01-18 15:15:58 +0900

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './footer.scss';
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <p>(주)포트코퍼레이션 대표 : 심상진  사업자등록번호 : 860-81-2559  주소 : 서울 양천구 모록로 138, 102호  전화 : 1544-8173</p>
+        <p>이용약관  개인정보처리방침  관리자</p>
+        <p>Copyright © 2022 포트캔커피 All rights reserved.</p>
+        <img src="/img/footercrown.png" alt="footer상훈" />
+      </footer>
+    </>
+  )
+}
+
+export default Footer
